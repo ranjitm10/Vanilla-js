151e0f39107ec394fcbd3c744b0224e0ed56924e
ranjitm10 <ranjitmohite00@gmail.com>
2021-03-20 11:49:33 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+// console.log("Lets see its working are not");
+// var h = prompt("Hello world");
+// console.log(h)
+// alert("let start vanilla js and dont stop")
+
+var buttont = document.querySelector(".button");
+var txtinput = document.querySelector(".txtarea");
+var input = document.querySelector(".btn-primary");
+var output = document.querySelector("#input-btn");
+document.querySelector("input[name=attritunjse]");
+
+buttont.addEventListener("click", function abc() {
+    console.log("LET see it working!");
+    console.log(txtinput.value);
+    console.log(input);
+    console.log(output);
+})
