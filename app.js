0950333189ee988dd2553926e3d179fff17e1d04
ranjitm10 <ranjitmohite00@gmail.com>
2021-03-22 22:30:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,11 +4,23 @@
 // alert("let start vanilla js and dont stop")
 
 var buttont = document.querySelector(".button");
-var txt = document.querySelector("#txt-area");
+var inputtxt = document.querySelector("#txt-area");
 var out = document.querySelector("#output");
 
+//server = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
-buttont.addEventListener("click", function abc() {
-    out.innerText = "heheh  " + txt.value;
-    console.log(out.innerText)
-})
+var server = "https://api.funtranslations.com/translate/pirate.json"
+function baseURL(text){
+    return server + "?" + "text=" + text
+}
+function abc(){
+    var input = inputtxt.value;
+
+    fetch(baseURL(input))
+    .then(response => response.json())
+    .then(json => console.log(json))
+
+};
+
+
+buttont.addEventListener("click",abc)
